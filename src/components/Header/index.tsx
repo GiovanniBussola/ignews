f80b1d8f1bb644cdcaeeef690e7e045808d2548b
Giovanni Bússola <giovanni.sbussola@gmail.com>
2021-12-29 09:25:50 -0300

    feat: module finalization

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,18 +1,14 @@
-import { useRouter } from 'next/router'
-import Link from 'next/link'
-import { SignInButton } from '../SignInButton'
-import styles from './styles.module.scss'
-import { ActiveLink } from '../ActiveLink'
+import { SignInButton } from "../SignInButton";
+import styles from "./styles.module.scss";
+import { ActiveLink } from "../ActiveLink";
 
 export function Header() {
-  const { asPath } = useRouter()
-
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
         <img src="/images/logo.svg" alt="ig.news" />
         <nav>
-          <ActiveLink activeClassName={styles.active} href="/" >
+          <ActiveLink activeClassName={styles.active} href="/">
             <a className={styles.active}>Home</a>
           </ActiveLink>
           <ActiveLink activeClassName={styles.active} href="/posts">
@@ -22,5 +18,5 @@ export function Header() {
         <SignInButton />
       </div>
     </header>
-  )
-}                     
+  );
+}
